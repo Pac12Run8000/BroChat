2a9c00206f4424d5fecc74fc81996bc59c6675f7
Pac12Run8000 <norbertgrover@michelles-macbook-pro.local>
2019-03-22 14:03:05 -0700

    feat: Added the dismiss functionality to the LoginViewController

--- BroChat/Controllers/LoginController.swift
+++ BroChat/Controllers/LoginController.swift
@@ -9,13 +9,35 @@
 import UIKit
 
 class LoginController: UIViewController {
-
+    
+    @IBOutlet weak var segmentOutlet: UISegmentedControl!
+    @IBOutlet weak var textFieldContainerView: UIView!
+    @IBOutlet weak var usernameOutlet: UITextField!
+    @IBOutlet weak var emailOutlet: UITextField!
+    @IBOutlet weak var passwordOutlet: UITextField!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-      setbackground()
+        segmentOutlet.selectedSegmentIndex = LoginState.Login.rawValue
+        setFieldsOnUI(segmentedIndex: segmentOutlet.selectedSegmentIndex)
+        setviews()
+        setimageView()
+        
+        passwordOutlet.delegate = self
+        emailOutlet.delegate = self
+        usernameOutlet.delegate = self
         
         
+    }
+    
+    @IBAction func segmentAction(_ sender: Any) {
+        
+        setFieldsOnUI(segmentedIndex: segmentOutlet.selectedSegmentIndex)
+        
+
         
     }
     
@@ -27,10 +49,53 @@ class LoginController: UIViewController {
 // MARK:- UI Layout
 extension LoginController {
     
-    private func setbackground() {
+    private func setimageView() {
+        imageView.layer.borderColor = UIColor.customDarkBlue.cgColor
+        imageView.layer.borderWidth = 4
+        imageView.layer.cornerRadius = 10
+        imageView.layer.masksToBounds = true
+    }
+    
+    private func setviews() {
         view.backgroundColor = UIColor.lightPinkish
+        textFieldContainerView.backgroundColor = UIColor.lightPinkish
+    }
+    
+    private func setFieldsOnUI(segmentedIndex:Int) {
+        usernameOutlet.isHidden = segmentedIndex == 0 ? true : false
+        imageView.isHidden = segmentedIndex == 0 ? true : false
+        emailOutlet.isHidden = false
+        passwordOutlet.isHidden = false
+        
+        if segmentedIndex == 0 {
+            emailOutlet.becomeFirstResponder()
+        } else {
+            usernameOutlet.becomeFirstResponder()
+        }
     }
     
     
 }
+// MARK:- Enum for the controller state - Login, Register
+extension LoginController {
+    
+    enum LoginState: Int {
+        case Login = 0
+        case Register = 1
+    }
+    
+
+    
+}
+
+// MARK:- UITextFieldDelegate functionality
+extension LoginController:UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.dismiss(animated: true, completion: nil)
+        return true
+    }
+}
+
+
 
