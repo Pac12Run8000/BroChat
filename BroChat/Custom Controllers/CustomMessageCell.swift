40284ed9fa65ba190e6473d14ae2f033b0227108
Pac12Run8000 <norbertgrover@michelles-macbook-pro.local>
2019-04-05 07:08:46 -0700

    feat: Formatted the date label

--- BroChat/Custom Controllers/CustomMessageCell.swift
+++ BroChat/Custom Controllers/CustomMessageCell.swift
@@ -13,11 +13,12 @@ class CustomMessageCell: UITableViewCell {
     @IBOutlet weak var userLabel: UILabel!
     @IBOutlet weak var CustomImageView: UIImageView!
     @IBOutlet weak var messageLabelOutlet: UILabel!
+    @IBOutlet weak var timestampLabel: UILabel!
     
-    var userObj:User! {
+    var userObj:User? {
         didSet {
-            userLabel.text = userObj.username
-            if let profileImageUrl = userObj.profileImageUrl, let url = URL(string: profileImageUrl) {
+            userLabel.text = userObj!.username
+            if let profileImageUrl = userObj!.profileImageUrl, let url = URL(string: profileImageUrl) {
                 ImageService.downloadAndCacheImage(withUrl: url) { (succees, image, error) in
                     self.CustomImageView.image = image
                 }
@@ -25,14 +26,29 @@ class CustomMessageCell: UITableViewCell {
         }
     }
     
-    var messageObj:Message! {
+   
+    
+    var messageObj:Message? {
         didSet {
-            messageLabelOutlet.text = messageObj.text
+            
+            messageLabelOutlet.text = messageObj!.text
+            
+            if let seconds = messageObj?.timestamp?.doubleValue ,let timestampDate = NSDate(timeIntervalSince1970: seconds) as? NSDate {
+                let dateFormatter = DateFormatter()
+                dateFormatter.dateFormat = "hh:mm:ss a"
+                timestampLabel.text = dateFormatter.string(from: timestampDate as Date)
+                
+            }
+            
         }
     }
 
     override func awakeFromNib() {
         super.awakeFromNib()
+//        userLabel.layer.borderWidth = 2
+//        timestampLabel.layer.borderWidth = 2
+//        timestampLabel.font = UIFont(name: "Arial", size: 12)
+        timestampLabel.textColor = UIColor.customDarkBlue
        
         CustomImageView.image = UIImage(named: "addImage-3")
         CustomImageView.layer.cornerRadius = CustomImageView.frame.height / 2
